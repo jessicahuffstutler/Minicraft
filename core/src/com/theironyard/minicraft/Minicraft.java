90491616fc464f55a4410b2945356a652347e82c
jessicahuffstutler <jessica.huffstutler@gmail.com>
2015-10-23 11:44:51 -0400

    commit for basic project submission

--- core/src/com/theironyard/minicraft/Minicraft.java
+++ core/src/com/theironyard/minicraft/Minicraft.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 public class Minicraft extends ApplicationAdapter {
     final int WIDTH = 100;
@@ -14,13 +15,19 @@ public class Minicraft extends ApplicationAdapter {
 
     SpriteBatch batch;
     TextureRegion down, up, right, left;
+    FitViewport viewport;
 
     float x = 0;
     float y = 0;
+    float xVelocity = 0;
+    float yVelocity = 0;
+
+    final float MAX_VELOCITY = 100;
 
     @Override
     public void create () {
         batch = new SpriteBatch();
+        viewport = new FitViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
         Texture tiles = new Texture("tiles.png");
         TextureRegion[][] grid = TextureRegion.split(tiles, 16, 16);
@@ -34,13 +41,12 @@ public class Minicraft extends ApplicationAdapter {
     @Override
     public void render () {
         move();
+        draw();
+    }
 
-        Gdx.gl.glClearColor(0, 0.5f, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        batch.begin();
-        batch.draw(down, x, y, WIDTH, HEIGHT);
-        batch.end();
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 
     void move() {
@@ -60,7 +66,44 @@ public class Minicraft extends ApplicationAdapter {
         x += xVelocity * Gdx.graphics.getDeltaTime(); //makes it move but takes into account pixel refresh rate
         y += yVelocity * Gdx.graphics.getDeltaTime();
 
+        //screen parameters
+        if (y < 0 ) { //player doesn't go below y axis
+            y = 0;
+        }
+
+        if (x < 0) { //player doesn't go to the left of x axis
+            x = 0;
+        }
+
+        if (y > viewport.getScreenHeight() - HEIGHT) { //player doesn't go past right side of screen
+            y = viewport.getScreenHeight() - HEIGHT;
+        }
+
+        if (x > viewport.getScreenWidth() - WIDTH) { //player doesn't go past top of screen
+            x = viewport.getScreenWidth() - WIDTH;
+        }
+
         xVelocity *= 0.9;
         yVelocity *= 0.9;
+    }
+
+    void draw() {
+        TextureRegion img;
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            img = left;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            img = up;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            img = right;
+        } else {
+            img = down;
+        }
+
+        Gdx.gl.glClearColor(0, 0.5f, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(img, x, y, WIDTH, HEIGHT);
+        batch.end();
     }
 }
